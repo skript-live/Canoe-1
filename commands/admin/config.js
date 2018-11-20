1bccdf2276699d0fe1007a545c755dbfb9953781
MrScopes <drewmanrules23@gmail.com>
2018-11-20 09:57:07 -0500

    Update config.js

--- commands/admin/config.js
+++ commands/admin/config.js
@@ -1,83 +1,68 @@
-
+const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
 	const conf = client.settings.ensure(message.guild.id, defaultS);
-	if (!message.member.hasPermission('MANAGE_SERVER')) {
-		let noAccess = client.settings.get(message.guild.id, 'noAccess');
-		message.channel.send(noAccess);
-		return;
-	}
+	if (!message.member.hasPermission('MANAGE_SERVER')) return message.channel.send(`You don't have access to that.`);
 	if (!args[0] && !args[1]) {
-		message.channel.send([
-			conf.prefix,
-			conf.noAccess,
-			conf.modRole,
-			conf.welcome,
-			conf.leave,
-			conf.messageDelete
-		])
-		return;
+		const configuration = new Discord.RichEmbed()
+			.setTitle(message.guild.name + ' configuration')
+			.setAuthor(message.guild.name, message.guild.icon)
+			.setImage(message.guild.icon)
+			.setColor('#FF5533')
+			.setDescription([
+				`\n`,
+				`\`Prefix\`: \`${conf.prefix}\``,
+				`\`ModRole\`: \`${conf.modRole}\``,
+				`\`MutedRole\`: \`${conf.muteRole}\``,
+				`\`Logs\`: \`${conf.logs}\``,
+				`\`Greeting\`: \n${conf.greeting}`,
+				`\`Farewell\`: \n${conf.farewell}`,
+				``
+			])
+		return message.channel.send(configuration)
 	}
 	if (args[0] === 'reset') {
 		client.settings.delete(message.guild.id);
-		return await message.channel.send([
-			`:white_check_mark: All Configuration has been reset`,
-			`My prefix is now \`can (help)\``
-		])
+		return message.channel.send(`Successfully reset all configuration for \`${guild.name}\`.`
 	}
 	if (args[0] === 'prefix') {
 		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the prefix.')
+			client.settings.delete(message.guild.id, `prefix`);
+			return message.channel.send(`Successfully reset the prefix.`)
 		}
-		if (args[1].length > 8) return message.channel.send('The length of the prefix must be < 8')
-		client.settings.set(message.guild.id, args[1], 'prefix');
-		message.channel.send(`:white_check_mark: Updated Configuration...`)
-		return;
+		if (args.join().slice(1).length > 8) return message.channel.send(`The prefix must be below 8 characters.`)
+		client.settings.set(message.guild.id, args.join().slice(1), `prefix`);
+		return message.channel.send(`Successfully updated the prefix to \`${args.join().slice(1)}\`.`;
 	}
-	if (args[0] === 'noAccess') {
+	if (args[0] === 'modrole') {
 		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, noAccess);
-			return message.channel.send (':white_check_mark: Reset the noAccess message.')
-		}
-		return;  
-	}
-	if (args[0] === 'modRole') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, modRole);
-			return message.channel.send (':white_check_mark: Reset the modRole.')
+			client.settings.delete(message.guild.id, `modRole`);
+			return message.channel.send ('Successfully reset the ModRole.')
 		}
 		return;  
 	}
 	if (args[0] === 'logs') {
 		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the logs channel.')
+			client.settings.delete(message.guild.id, `logs`);
+			return message.channel.send ('Successfully disabled all logging.')
 		}
 		return;  
 	}
-	if (args[0] === 'welcome') {
+	if (args[0] === 'greeting') {
 		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the welcome message.')
+			client.settings.delete(message.guild.id, `greeting`);
+			return message.channel.send ('Successfully disabled member greeting.')
 		}
 		return;  
 	}
-	if (args[0] === 'leave') {
+	if (args[0] === 'farewell') {
 		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the farewell message.')
-		}
-		return;  
-	}
-	if (args[0] === 'messageDelete') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the messageDelete message.')
+			client.settings.delete(message.guild.id, `farewell`);
+			return message.channel.send ('Successfully disabling member farewells.')
 		}
 		return;  
 	}
 	else {
-	  return message.channel.send('Read my documentation: https://canoe.forums.gg/docs')
+		return;
 	}
 };
