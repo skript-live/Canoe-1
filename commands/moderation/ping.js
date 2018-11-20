7304bc0661bfbbb8c42dc20fa9f1ebd2d7a27d2c
MrScopes <drewmanrules23@gmail.com>
2018-11-20 10:01:07 -0500

    Delete ping.js

--- commands/moderation/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (client, message, args) => {
-    msg = await message.channel.send('Fetching Ping...');
-    await msg.edit([
-        `**PING:** \`${MSG.createdTimestamp - message.createdTimestamp}ms\``,
-        `**DiscordAPI:** \`${Math.round(client.ping)}ms\``
-    ])
-}
