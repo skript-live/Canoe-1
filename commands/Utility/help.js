2180a099e06d96473e9cf4c324acdcc4138168d4
MrScopes <drewmanrules23@gmail.com>
2018-11-20 10:01:31 -0500

    Delete help.js

--- commands/Utility/help.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = async (client, message, args) => {
-	const conf = client.settings.ensure(message.guild.id, defaultS);
-	const prefix = conf.prefix;
-	message.channel.send('You can find a command reference here: ')
-}
