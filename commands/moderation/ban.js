380720a75ff7a9fd066bbde39e71f69c4af51788
MrScopes <drewmanrules23@gmail.com>
2018-11-20 00:16:29 -0500

    Add files via upload

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 module.exports.run = async (client, message, args) => {
 	const conf = client.settings.ensure(message.guild.id, defaultS);
 	const modRole = message.guild.roles.find(r => r.name === conf.modRole);
@@ -9,8 +11,23 @@ module.exports.run = async (client, message, args) => {
 	let user = message.mentions.members.first();
 	if (!user) user = message.guild.members.get(args[0]);
 	if (!user) return message.channel.send(`Couldn't convert '\`${args[0]}\`' to type \`guildMember\`.`);
-	if (!user.managable) return message.channel.send(`:no_entry_sign: Insufficient Permissions`);
+	if (!user.kickable) return message.channel.send(`:no_entry_sign: Insufficient Permissions`);
 	const reason = args.slice(1).join(' ');
-	await user.ban(reason);
+	user.send([
+		`Hi`,
+		``,
+		`You've received a **__Ban__** on **${message.guild.name}**`,
+	])
+	const banned = new Discord.RichEmbed()
+		.setTitle(`Moderation action on ${message.guild.name}`)
+		.setDescription('A moderation action has been taken against you')
+		.setFooter('This is an automated message. Contact the moderators for more information')
+		.setColor('#7F0E01')
+		.addField('Action', 'Banned', true)
+		.addField('Moderator', message.member.name, true)
+		.addField('Reason', reason, true)
+		.addField('Expires', 'Never')
+	await user.ban(reason).catch(() => 'e');
 	message.channel.send(`:ok_hand: Successfully banned ${user.user.tag}.`)
+	await user.send(banned).catch(() => 'e');
 };
--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,4 +1,4 @@
 module.exports = (client, user, guild) => {
-    log = message.guild.channels.find(c => c.name === client.settings.get(message.guild.id, 'logs'))
+    log = guild.channels.find(c => c.name === client.settings.get(guild.id, 'logs'))
     if (log) return log.send(`:wizard: ${user.tag} \`(${user.id})\` has been **unbanned**. `)
 };
