c07eaedcc5885702b794d9655caad89253d2356a
MrScopes <drewmanrules23@gmail.com>
2018-11-20 00:51:10 -0500

    Add files via upload

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,6 +1,6 @@
 module.exports = (client, message) => {
 	if (message.author.bot || !message.guild) return;
-	log = message.guild.channels.find(c => c.name === client.settings.get(message.guild.id, 'logs'))
+	const log = message.guild.channels.find(c => c.name === client.settings.get(message.guild.id, 'logs'))
 	if (log) {
 		log.send([
 			`:wastebasket: ${message.author.tag} \`(${message.author.id})\` message deleted in **#${message.channel.name}**:`,
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,14 +1,10 @@
 module.exports = (client, oldMessage, newMessage) => {
-	console.log('1')
-	if (message.author === client.user || !message.guild) return;
-	console.log('2')
+	if (oldMessage.author === client.user || !oldMessage.guild) return;
 	if (oldMessage.content === newMessage.content) return;
-	console.log('3')
-	log = message.guild.channels.find(c => c.name === client.settings.get(message.guild.id, 'logs'))
-	console.log(log)
+	const log = oldMessage.guild.channels.find(c => c.name === client.settings.get(oldMessage.guild.id, 'logs'))
 	if (log) {
 		log.send([
-			`:pencil: ${oldMessage.author.tag} \`(${oldMessage.author.id})\` message edit in **#${oldMessage.channel.name}**:`,
+			`:pencil: ${oldMessage.author.tag} \`(${oldMessage.author.id})\` message edited in **#${oldMessage.channel.name}**:`,
             `**B:** ${oldMessage.cleanContent}`,
             `**A:** ${newMessage.cleanContent}`,
 		])
